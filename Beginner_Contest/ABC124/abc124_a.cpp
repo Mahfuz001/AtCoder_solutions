3394d730b8eb83e2007d2a16ec506a73a6bf0441
Mahfuz001 <likhon.codew@gmail.com>
2019-05-19 14:10:46 +0600

    Repo Organized

---
+++ Beginner_Contest/ABC124/abc124_a.cpp
@@ -0,0 +1,38 @@
+/**
+ * 	author: krayr
+ * 	created: 19.05.2019
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define all(a) (a).begin(), (a).end()
+#define sz(a) (int)(a).size()
+#define pb push_back
+#define mp make_pair
+#define forn(i, s, n) for(int i = s; i < int(n); i++)
+
+typedef long long ll;
+//~ const double pi = 2*acos(0.0);
+//~ const long long mod = 1e9+7;
+//~ const int MAX_N = 1e6+5;
+
+int main(int argc, char* argv[]){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+       
+    #ifdef _DEBUG
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    #endif
+    
+	int a, b; cin >> a >> b;
+	
+	int x = max(a, b);
+	
+	int ans = max((x + (x-1)), (a+b));
+	
+	cout << ans << "\n";
+    
+
+    return 0;
+}
